573e2fee5189c39a0a874dab7a9cf95324c3b9db
Srikar Thottempudi <srikarthottempudi@Srikars-Air.home>
2019-02-20 21:37:50 -0500

    added nav controller

--- MYOA/ViewController.swift
+++ MYOA/ViewController.swift
@@ -13,8 +13,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Start Over", style: .plain, target: self, action: #selector(startOver))
     }
 
+    @objc func startOver() {
+        if let navigationController = navigationController {
+            navigationController.popToRootViewController(animated: true)
+        }
+    }
+    
+    deinit {
+        print("deinitializer is called.")
+    }
 
 }
 
